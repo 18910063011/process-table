93b935e05de118bb9aa0ce9c570171c3e04ed2fc
Auto Deploy <deploy@github.com>
2026-04-09 19:13:13 +0800

    feat: 进程管理表格应用

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: './',
+  build: {
+    assetsDir: 'assets',
+    outDir: 'dist',
+  },
+  server: {
+    port: 5000,
+    host: '0.0.0.0',
+    allowedHosts: true,
+    hmr: {
+      overlay: true,
+      path: '/hot/vite-hmr',
+      port: 6000,
+      clientPort: 443,
+      timeout: 30000,
+    },
+    watch: {
+      usePolling: true,
+      interval: 100,
+    }
+  },
+});
